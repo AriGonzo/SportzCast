daecb5433109ff5850cc7d8271b679944f03ed87
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2015-02-24 11:59:09 -0500

    sizing issue

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -4,18 +4,17 @@ angular.module('sportzCast')
     $(document).ready(function() {
       $('#navSep').hide();
     });
-    var BV = new $.BigVideo();
-    BV.init();
-    if (Modernizr.touch){
-      BV.show('http://i.imgur.com/3m1OkPZ.jpg')
-    } else {
-      BV.show([
-        { type: 'video/mp4', src:"assets/images/scoreboard.mp4" },
-        { type: 'video/webm', src:"assets/images/scoreboard.webm"},
-        ],{ambient:true});
-    }
+    // var BV = new $.BigVideo();
+    // BV.init();
+    // if (Modernizr.touch){
+    //   BV.show('http://i.imgur.com/3m1OkPZ.jpg')
+    // } else {
+    //   BV.show([
+    //     { type: 'video/mp4', src:"assets/images/scoreboard.mp4" },
+    //     { type: 'video/webm', src:"assets/images/scoreboard.webm"},
+    //     ],{ambient:true});
+    // }
   this.stopVid = function(){
-    BV.getPlayer().dispose()
     $state.go('browse')
     $('.navbar').show();
     }  
