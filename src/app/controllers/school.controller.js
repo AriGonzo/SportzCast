7473b7667273586cbd793446b72c7c03fc81c439
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2015-03-19 11:20:09 -0400

    added twitter stream

--- src/app/controllers/school.controller.js
+++ src/app/controllers/school.controller.js
@@ -9,8 +9,10 @@ angular.module('sportzCast')
   		console.log(this.showTab)
   	}
   	
-  	console.log($stateParams)
-
+  	// var getTweets = function(){
+   //    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+   //  }
+    
   	//API call
   	var baseUrl = SportzCastApi.url('schools');
   	this.schoolInfo = {};
@@ -19,6 +21,7 @@ angular.module('sportzCast')
   		self.schoolInfo = data
   		$('.schoolBanner').css({"background-color":self.schoolInfo.PrimaryColor})
   		$('.schoolBanner').css({"color":self.schoolInfo.SecondaryColor})
+      console.log(self.schoolInfo.Twitter)
   		if (!self.schoolInfo.Sponsor){
         self.schoolInfo.Sponsor = {}
         self.schoolInfo.Sponsor.FallBack = "AdSense"
