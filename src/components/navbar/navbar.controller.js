4e1190b5b7e1caa845834f0f0f90fadd4c28772f
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2015-03-22 17:50:54 -0400

    autoclose menu

--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -2,5 +2,8 @@
 
 angular.module('sportzCast')
   .controller('NavbarCtrl', function ($state) {
-  	
+
+  		$(".navbar-nav li a").click(function(event) {
+    		$(".navbar-collapse").collapse('hide');
+  		});
   });
